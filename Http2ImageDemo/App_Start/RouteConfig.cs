ff6f956e783e7bee1b210fef17886472425f29f8
Ben Dewey <bdewey01@hotmail.com>
2016-05-19 08:17:03 -0500

    Initial Commit

---
+++ Http2ImageDemo/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Http2ImageDemo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            //routes.MapRoute(
+            //    name: "DefaultDemo",
+            //    url: "Demo/{image}",
+            //    defaults: new { controller = "Home", action = "Index", image = UrlParameter.Optional }
+            //);
+            
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{image}",
+               defaults: new { controller = "Home", action = "Index", image = UrlParameter.Optional }
+           );
+        }
+    }
+}
---
+++ Http2ImageDemo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Http2ImageDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string image)
+        {
+            base.HttpContext.Response.PushPromise("/Content/Site.css");
+
+            ViewBag.Image = image;
+            ViewBag.Sheme = base.HttpContext.Request.Url.Scheme;
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
